a99569561c0dd600a4385b978135562138bda074
mdivani <georgemdivani.gm@gmail.com>
2018-12-25 15:27:28 +0400

    fix indentions

--- app/scrapper/scrapeFilters.js
+++ app/scrapper/scrapeFilters.js
@@ -2,35 +2,35 @@ const { getTarget } = require("./index");
 const { getFilters, filterParser, flattenArray } = require("./helper");
 
 const scrapeFilters = (filterName="cat") => {
-                       return  getFilters(filterName)
-                        .then((filters) => {
-                          const filterPromises = filters.map((filter) => {
-                            const route = `?page=1&${filterName}=${filter.val}`;
-                            return getTarget(route)
-                                .then((scrapper) => {
-                                // check length of pages for current filter
-                                const pages = scrapper(".pagebox").length + 1;
-                                const pagePromises = [];
+    return  getFilters(filterName)
+    .then((filters) => {
+        const filterPromises = filters.map((filter) => {
+        const route = `?page=1&${filterName}=${filter.val}`;
+        return getTarget(route)
+            .then((scrapper) => {
+            // check length of pages for current filter
+            const pages = scrapper(".pagebox").length + 1;
+            const pagePromises = [];
 
-                                for (let i = 1; i <= pages; i++) {
+            for (let i = 1; i <= pages; i++) {
 
-                                    const route = `/?page=${i}&${filterName}=${filter.val}`;
-                                    pagePromises[i-1] = getTarget(route)
-                                                        .then((scrapper) => {
-                                                            return filterParser({
-                                                                            scrapper, 
-                                                                            filter: filter.text, 
-                                                                            filterName
-                                                                        });
-                                                                    });
-                                }
-                                return Promise.all(pagePromises);
-                                });
-                            });
-                            return Promise.all(filterPromises);
-                        })
-                        .then((data) => flattenArray(data))
-                        .catch((error) => console.error(error));
-                    }
+                const route = `/?page=${i}&${filterName}=${filter.val}`;
+                pagePromises[i-1] = getTarget(route)
+                                    .then((scrapper) => {
+                                        return filterParser({
+                                                        scrapper, 
+                                                        filter: filter.text, 
+                                                        filterName
+                                                    });
+                                                });
+            }
+            return Promise.all(pagePromises);
+            });
+        });
+        return Promise.all(filterPromises);
+    })
+    .then((data) => flattenArray(data))
+    .catch((error) => console.error(error));
+}
 
 module.exports = scrapeFilters;
--- app/scrapper/scrapePages.js
+++ app/scrapper/scrapePages.js
@@ -5,11 +5,11 @@ const { flattenArray, pageParser, parseArrays, getScrapperArr } = require("./hel
 // parses through each page and collects data
 // finally flattens array of arrays into one array and returns list of all available jobs on site
 const scrapePages = (scrapper) => {
-                                const scrapperArr = getScrapperArr({scrapper});
-                                return Promise.all(scrapperArr)
-                                              .then((pagesArr) => parseArrays(pagesArr, pageParser))
-                                              .then((totalLists) => flattenArray(totalLists))
-                                              .catch((error) => console.error(error));
-                            }
+    const scrapperArr = getScrapperArr({scrapper});
+    return Promise.all(scrapperArr)
+                    .then((pagesArr) => parseArrays(pagesArr, pageParser))
+                    .then((totalLists) => flattenArray(totalLists))
+                    .catch((error) => console.error(error));
+}
 
 module.exports = scrapePages;
